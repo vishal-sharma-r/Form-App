65e2fddc13c92a23703cddff9347b5b7a00ac185
“vishal-sharma-r” <sharmaji20rattan@gmail.com>
2023-10-30 15:56:53 +0530

    form app

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='grid justify-center text-3xl h-screen text-richblack-100  place-items-center'>Dashboard</div>
+  )
+}
+
+export default Dashboard
---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='grid justify-center text-3xl h-screen text-richblack-100  place-items-center'>Home</div>
+  )
+}
+
+export default Home
